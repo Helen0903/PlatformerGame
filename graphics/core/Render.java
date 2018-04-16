6eecd237260abcd4bcca3d2e268f5a408a41300b
Helen <helen0903brown@gmail.com>
2018-04-16 21:17:11 +0100

    Added Render.java
    Added Render class in order to allow correct usage of the Canvas class.
    This class allows for implementation of any code (like a Runnable) with a Graphics parameter.

---
+++ graphics/core/Render.java
@@ -0,0 +1,6 @@
+public abstract class Render {
+    /**
+     * The abstract function in which the programmer decides what is to be done on each frame.
+     */
+    public abstract draw(Graphics g);
+}
